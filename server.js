afb42998184407368068ba785ad2b298e318f587
ronchitech <ronchibusiness@gmail.com>
2021-08-10 21:04:00 +0800

    separated the app and server

---
+++ server.js
@@ -0,0 +1,18 @@
+const http = require('http')
+const app = require('./app')
+const mongoose = require('mongoose')
+const MONGODB_URI = process.env.MONGODB_URI;
+
+
+const server = http.createServer(app)
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    server.listen(process.env.PORT, () => {
+      console.log(`Server is running at port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
